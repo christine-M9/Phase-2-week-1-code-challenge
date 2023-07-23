a0dbf707737721745f90e362f6d4b2de43ae6a3e
Christine <christinekhekama99@gmail.com>
2023-07-24 00:52:33 +0300

    updating the index.js

--- bank-of-flatiron/src/index.js
+++ bank-of-flatiron/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './index.css'; 
 import App from './App';
 
-
-
-ReactDOM.render(<App />, document.getElementById("roots"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
